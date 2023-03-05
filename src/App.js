4521303dddcf8e036dfc9b9ebe742b428547b05d
Bryce Fisher <bryce.e.fisher@gmail.com>
2023-03-04 19:07:50 -0800

    First Comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import HamburgerMenu from "react-hamburger-menu";
+import "./index.css";
 
 function App() {
+  const [isPaneOpen, setPaneOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+
+  const handleClick = () => {
+    setPaneOpen(!isPaneOpen);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+      setPaneOpen(true);
+    } else {
+      setPaneOpen(false);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={`App-header ${isPaneOpen ? "hidden" : ""}`}>
+        <HamburgerMenu
+          isOpen={isPaneOpen}
+          menuClicked={handleClick}
+          width={35}
+          height={35}
+          strokeWidth={1}
+          rotate={0}
+          color="white"
+          borderRadius={0}
+          animationDuration={0.5}
+        />
       </header>
+
+      {isPaneOpen && (
+        <div className="pane">
+          <div className="close-menu">
+            <HamburgerMenu
+              isOpen={isPaneOpen}
+              menuClicked={handleClick}
+              width={35}
+              height={35}
+              strokeWidth={1}
+              rotate={0}
+              color="white"
+              borderRadius={0}
+              animationDuration={0.5}
+              isCross
+            />
+          </div>
+          <div className="pane-content">
+            <ol id="navlistPane" type="1">
+              <li>
+                <a className="nav" href="index.html">
+                  About
+                </a>
+              </li>
+              <li>
+                <div>
+                  <a className="nav" href="#stuff">
+                    Stuff
+                  </a>
+                </div>
+              </li>
+              <li>
+                <a className="nav" href="#work">
+                  Work
+                </a>
+              </li>
+              <li>
+                <a className="nav" href="#contact">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  className="nav"
+                  target=" _blank"
+                  id="resume"
+                  href="resume.pdf"
+                >
+                  Resume
+                </a>
+              </li>
+            </ol>
+          </div>
+          <div className="backdrop" onClick={handleClick}></div>
+        </div>
+      )}
     </div>
   );
 }
